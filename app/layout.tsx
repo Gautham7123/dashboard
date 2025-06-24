d6be425d361ae0309c6a00b109cec3a52e0c0d57
Gautham <160218733+Gautham7123@users.noreply.github.com>
2025-06-24 18:08:34 +0530

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+import { Metadata } from 'next';
  
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
   );
 }
